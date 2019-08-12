4f9f09aca36a50d229af4a0481ead8a195a926c2
Joao Correia <joao.correia@pitang.com>
2019-08-11 22:53:41 -0300

    add config.ini to the database

--- SearchStringAndCreateLogs/db_api/db_fabric.py
+++ SearchStringAndCreateLogs/db_api/db_fabric.py
@@ -1,9 +1,15 @@
 import psycopg2
+import configparser
+
 
 class dbFabric():
 
     @staticmethod
     def connection():
-        db = psycopg2.connect("dbname='treinaweb_clientes' user='postgres' host='localhost' "
-                                             "password='12345'")
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        db = psycopg2.connect(dbname=config['DATABASE']['dbname'],
+                              user=config['DATABASE']['user'],
+                              host=config['DATABASE']['host'],
+                              password=config['DATABASE']['password'])
         return db
