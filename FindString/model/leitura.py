f6b2eede76e85f75bad955366f2af535429282e0
Joao Correia <joao.correia@pitang.com>
2019-08-21 19:47:11 -0300

    Mudança do retorno do metodo de dict to list

--- FindString/model/leitura.py
+++ FindString/model/leitura.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 from pathlib import Path
+import shutil
 
 class Leitura(object):
     def __init__(self, pasta_principal):
@@ -39,15 +40,15 @@ class Leitura(object):
 
     def listar_projetos(self):
         diretorios = self.__lista_diretorios()
-        retorno = {}
+        retorno = list()
         for root in diretorios:
             path = self.__pasta_principal + "/" + root.name
-            retorno[root] = self.__lista_diretorios(path)
+            retorno.append({path: self.__lista_diretorios(path)})
 
-        self.__escrever_log("-------------------------------------------------------")
+        self.__escrever_log("-=" *15)
         self.__escrever_log("Lendo Diretorios..." + datetime.datetime.now().__str__())
         self.__escrever_log(retorno.__str__())
-        self.__escrever_log("-------------------------------------------------------")
+        self.__escrever_log("-=" *15)
 
         return retorno
 
@@ -60,11 +61,19 @@ class Leitura(object):
     def is_mais_atual(self):
         self.__escrever_log("Determinando quais diretorios são os mais atuais.......")
         list_diretorios = self.listar_projetos()
-        for i,r in list_diretorios.items():
-            for second in r:
-                print(second)
+
+        path_aux_maior = list_diretorios[0].copy()
+        print(f"Lista template de Comparação:{path_aux_maior}")
+        for i in range(1, len(list_diretorios)):
+            print(list_diretorios[i])
 
 
+    #exemplo
+    # lista_final =list(set(lista_1) - set(lista_2)) nao irá retornar elementos repetidos
+    # or [x for x in lista_2 if x not in lista_1] caso queria retornar elementos repetidos
+
+    def copy_to_temp(self, from_path, to_path):
+        shutil.copytree(from_path, to_path)
 
 if __name__ == '__main__':
     leitura = Leitura("C://Projetos")
