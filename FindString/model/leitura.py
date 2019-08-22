ef3312fc0d47a3b9b9b3381d809b231fca0da8dc
Joao Correia <joao.correia@pitang.com>
2019-08-21 22:03:01 -0300

    Mudança do retorno do metodo de dict to list

--- FindString/model/leitura.py
+++ FindString/model/leitura.py
@@ -4,17 +4,15 @@ from pathlib import Path
 import shutil
 
 class Leitura(object):
-    def __init__(self, pasta_principal):
-        self.__pasta_principal = pasta_principal
-        self.__criar_log()
+    def __init__(self, diretorio ,pasta):
+        self.__diretorio = diretorio
+        self.__pasta = pasta
+        self.__path_completo = diretorio + "://" + pasta
 
     def __criar_log(self):
-        if os.path.exists(self.__pasta_principal + "/LOG"):
-            caminho = self.__pasta_principal + "/LOG/log.txt"
-        else:
-            os.mkdir(self.__pasta_principal + "/LOG", 1)
-            self.__criar_log()
-        self.__caminho = caminho
+        p = Path(self.__path_completo + "/LOG")
+        p.mkdir(exist_ok=True)
+        self.__caminho = self.__path_completo + "/LOG/log.txt"
 
     def __escrever_log(self, texto):
         if self.__verificarFile():
@@ -23,9 +21,14 @@ class Leitura(object):
             arquivo.close()
         else:
             arquivo = open(self.__caminho, 'w')
-            arquivo.write("######################################################################\n")
-            arquivo.write("########################### INICIO DO LOG ############################\n")
-            arquivo.write("######################################################################\n")
+            arquivo.write("#" * 54 )
+            arquivo.write("\n")
+            arquivo.write("#" * 20)
+            arquivo.write(" Inicio - LOG ")
+            arquivo.write("#" * 20)
+            arquivo.write("\n")
+            arquivo.write("#" * 54 )
+            arquivo.write("\n")
             arquivo.close()
             self.__escrever_log(texto)
 
@@ -36,19 +39,45 @@ class Leitura(object):
         if path is not None:
             return Path(path).iterdir()
         else:
-            return Path(self.__pasta_principal).iterdir()
+            return Path(self.__path_completo).iterdir()
+
+    def create_folder_Eleita(self):
+        list = self.__lista_diretorios()
+        self.__criar_log()
+        to_path = self.__diretorio + "://z__Eleita"
+        p = Path(to_path)
+        p.mkdir(exist_ok=True)
+        winner = []
+        self.__escrever_log("-=" * 30)
+        self.__escrever_log("Lendo Diretorios..." + datetime.datetime.now().__str__())
+        self.__escrever_log("-=" * 30)
+        for d in list:
+            self.__escrever_log(f"Lendo sub_diretorio: {d.absolute()} ")
+            if d.is_dir():
+                aux = [dc for dc in os.listdir(d.absolute()) if not os.path.isfile(os.path.join(d.absolute(), dc))]
+                self.__escrever_log(f"Lista: {aux} ")
+                for a in aux:
+                    if a not in winner:
+                        winner.append(a)
+                        from_path = self.__path_completo + "/" + d.absolute().name + "/"+ a
+                        _to_path = to_path + "/" + a
+                        self.__escrever_log(f"Copiando de: {from_path} para {_to_path}")
+                        self.copy_folder(from_path, _to_path)
+            self.__escrever_log("-=" * 30)
+        return to_path
+
 
     def listar_projetos(self):
         diretorios = self.__lista_diretorios()
         retorno = list()
         for root in diretorios:
-            path = self.__pasta_principal + "/" + root.name
+            path = self.__path_completo + "/" + root.name
             retorno.append({path: self.__lista_diretorios(path)})
 
-        self.__escrever_log("-=" *15)
+        self.__escrever_log("-=" *30)
         self.__escrever_log("Lendo Diretorios..." + datetime.datetime.now().__str__())
         self.__escrever_log(retorno.__str__())
-        self.__escrever_log("-=" *15)
+        self.__escrever_log("-=" *30)
 
         return retorno
 
@@ -56,7 +85,6 @@ class Leitura(object):
         data = datetime.datetime.utcfromtimestamp(timestamp)
         data_formatada = data.strftime('%d-%m-%Y %H:%M:%S')
         return data_formatada
-
 
     def is_mais_atual(self):
         self.__escrever_log("Determinando quais diretorios são os mais atuais.......")
@@ -67,15 +95,10 @@ class Leitura(object):
         for i in range(1, len(list_diretorios)):
             print(list_diretorios[i])
 
-
-    #exemplo
-    # lista_final =list(set(lista_1) - set(lista_2)) nao irá retornar elementos repetidos
-    # or [x for x in lista_2 if x not in lista_1] caso queria retornar elementos repetidos
-
-    def copy_to_temp(self, from_path, to_path):
+    def copy_folder(self, from_path, to_path):
         shutil.copytree(from_path, to_path)
 
 if __name__ == '__main__':
-    leitura = Leitura("C://Projetos")
+    leitura = Leitura("C","Projetos")
 
-    leitura.is_mais_atual()
+    leitura.create_folder_Eleita()
